32704200b009b19053597501c97894765d79664b
Vitor Pereira <vitorfmp670@gmail.com>
2025-09-13 10:24:03 -0300

    Arquivo math.js
    Funcionalidade soma, subtração, multiplicação e divisão implementados.

---
+++ src/math.js
@@ -0,0 +1,18 @@
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    if (b === 0) {
+        return "Erro: divisão por zero não é permitida!";
+    }
+    return a / b;
+}
